5ddc7e0b6f6e32525e31a8233aea0c937a19f738
Sultonov-557 <sultonovzerifboy@gmail.com>
2025-08-26 12:13:38 +0500

    added redirect

--- src/pages/atmos/PaymentOtpPage.tsx
+++ src/pages/atmos/PaymentOtpPage.tsx
@@ -5,49 +5,49 @@ import { useApplyPayment } from "../../hooks/usePayment";
 const { Title } = Typography;
 
 export default function PaymentOtpPage() {
-  const [form] = Form.useForm();
-  const [searchParams] = useSearchParams();
-  const navigate = useNavigate();
+	const [form] = Form.useForm();
+	const [searchParams] = useSearchParams();
+	const navigate = useNavigate();
 
-  const transaction_id = Number(searchParams.get("transaction_id"));
-  const { mutate, isPending: isLoading } = useApplyPayment();
+	const transaction_id = Number(searchParams.get("transaction_id"));
+	const { mutate, isPending: isLoading } = useApplyPayment();
 
-  const onSubmit = (values: any) => {
-    mutate(
-      { ...values, transaction_id },
-      {
-        onSuccess: () => {
-          message.success("To‘lov muvaffaqiyatli tasdiqlandi");
-          navigate("/success");
-        },
-        onError: () => {
-          message.error("OTP noto‘g‘ri kiritildi");
-        },
-      }
-    );
-  };
+	const onSubmit = (values: any) => {
+		mutate(
+			{ ...values, transaction_id },
+			{
+				onSuccess: (data: { redirect: string }) => {
+					message.success("To‘lov muvaffaqiyatli tasdiqlandi");
+					navigate(`/success?url=${data.redirect}`);
+				},
+				onError: () => {
+					message.error("OTP noto‘g‘ri kiritildi");
+				},
+			},
+		);
+	};
 
-  return (
-    <div className="max-w-md mx-auto p-6">
-      <Title level={3}> OTP Kod</Title>
-      <Form form={form} layout="vertical" onFinish={onSubmit}>
-        <Form.Item
-          label="OTP"
-          name="otp"
-          rules={[{ required: true, message: "OTP kodini kiriting" }]}
-        >
-          <Input placeholder="123456" maxLength={6} />
-        </Form.Item>
+	return (
+		<div className="max-w-md mx-auto p-6">
+			<Title level={3}> OTP Kod</Title>
+			<Form form={form} layout="vertical" onFinish={onSubmit}>
+				<Form.Item
+					label="OTP"
+					name="otp"
+					rules={[{ required: true, message: "OTP kodini kiriting" }]}
+				>
+					<Input placeholder="123456" maxLength={6} />
+				</Form.Item>
 
-        <Button
-          type="primary"
-          htmlType="submit"
-          loading={isLoading}
-          className="w-full"
-        >
-          Tasdiqlash
-        </Button>
-      </Form>
-    </div>
-  );
+				<Button
+					type="primary"
+					htmlType="submit"
+					loading={isLoading}
+					className="w-full"
+				>
+					Tasdiqlash
+				</Button>
+			</Form>
+		</div>
+	);
 }
--- src/pages/atmos/PaymentSuccess.tsx
+++ src/pages/atmos/PaymentSuccess.tsx
@@ -1,32 +1,26 @@
 import { useEffect } from "react";
+import { useSearchParams } from "react-router-dom";
 
-declare global {
-  interface Window {
-    Telegram?: any;
-  }
-}
 const PaymentSuccess = () => {
-  useEffect(() => {
-    const timer = setTimeout(() => {
-      if (window.Telegram?.WebApp) {
-        window.Telegram.WebApp.close();
-      } else {
-        window.close();
-      }
-    }, 5000);
+	const [searchParams] = useSearchParams();
+	const url = searchParams.get("url");
+	useEffect(() => {
+		const timer = setTimeout(() => {
+			if (url) window.location.replace(url);
+		}, 5000);
 
-    return () => clearTimeout(timer);
-  }, []);
-  return (
-    <div className="flex flex-col items-center justify-center min-h-screen bg-green-50">
-      <div className="bg-white p-6 rounded-2xl shadow-lg text-center">
-        <h1 className="text-2xl font-bold text-green-600 mb-4">
-          To‘lovingiz uchun rahmat! 🎉
-        </h1>
-        <p className="text-gray-600">Sahifa 5 soniyadan so‘ng yopiladi...</p>
-      </div>
-    </div>
-  );
+		return () => clearTimeout(timer);
+	}, []);
+	return (
+		<div className="flex flex-col items-center justify-center min-h-screen bg-green-50">
+			<div className="bg-white p-6 rounded-2xl shadow-lg text-center">
+				<h1 className="text-2xl font-bold text-green-600 mb-4">
+					To‘lovingiz uchun rahmat! 🎉
+				</h1>
+				<p className="text-gray-600">Sahifa 5 soniyadan so‘ng yopiladi...</p>
+			</div>
+		</div>
+	);
 };
 
 export default PaymentSuccess;
